d56815cc30857cc929ac11b78429ea462b1493a8
Matt Helton <mjoelhelton@me.com>
2018-11-14 13:01:32 -0500

    Adds ability to take extra arguments into addition and calculator functions

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
-const add = (num1, num2) => {
-    return num1 + num2
+const add = (...numbers) => {
+    answer = 0
+    for (let number of numbers) answer += number
+    return answer
 }
-
 const subtract = (num1, num2) => {
     return num1 - num2
 }
@@ -14,9 +15,10 @@ const divide = (num1, num2) => {
     return num1 / num2
 }
 
-const calc = (callback, num1, num2) => {
-   return callback(num1, num2)
+const calc = (callback, ...numbers) => {
+   return callback(...numbers)
 
 }
 
-console.log(calc(add, 3, 4))
+
+console.log(calc(add,3, 4, 5, 1000))
