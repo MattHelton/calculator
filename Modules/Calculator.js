20e78454927a91ddfc3bcecef3b95a5c1996f238
MattHelton <mjoelhelton@me.com>
2019-01-07 12:00:10 -0500

    Adds average function and button.

--- Modules/Calculator.js
+++ Modules/Calculator.js
@@ -1,34 +1,34 @@
-const multiply = (num1, num2) => {
-    return num1 * num2
+const calc = (callback, ...nums) => {
+   return callback(...nums)
 }
-
-const divide = (num1, num2) => {
-    return num1 / num2
-}
-
-const calc = (callback, ...numbers) => {
-   return callback(...numbers)
-
-}
-
-export function add (...numbers) {
+export function add (...nums) {
     answer = 0
-    for (let number of numbers) answer += number)
+    for (let num of nums) answer += num
     return answer
 }
-
-export function subtract (num1, num2) {
+export function subtract (...nums) {
+    answer = 0
 return num1 - num2
 }
-
 export function multiply (num1, num2) {
     return num1 * num2
 }
-
 export function divide (num1, num2) {
     return num1 / num2
 }
 
 export function calc (callback, ...numbers) {
     return callback(...numbers)
+}
+export function average (...nums) {
+    let sum = 0
+    let numNums = 0
+    for (let num of nums) {
+        sum += num
+        numNums += 1
+    }
+    return sum / numNums
+}
+export function calc (callback, ...nums) {
+    return callback(...nums)
 }
