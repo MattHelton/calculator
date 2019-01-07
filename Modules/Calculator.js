578578e330d8950056d91ff8ce4b694d75e44925
MattHelton <mjoelhelton@me.com>
2019-01-06 22:57:44 -0500

    Adds index.html, creates App.js, creates Modules directory, moves Calculator.js to modules and refactors the functions as exports.

---
+++ Modules/Calculator.js
@@ -0,0 +1,34 @@
+const multiply = (num1, num2) => {
+    return num1 * num2
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2
+}
+
+const calc = (callback, ...numbers) => {
+   return callback(...numbers)
+
+}
+
+export function add (...numbers) {
+    answer = 0
+    for (let number of numbers) answer += number)
+    return answer
+}
+
+export function subtract (num1, num2) {
+return num1 - num2
+}
+
+export function multiply (num1, num2) {
+    return num1 * num2
+}
+
+export function divide (num1, num2) {
+    return num1 / num2
+}
+
+export function calc (callback, ...numbers) {
+    return callback(...numbers)
+}
