372889b58084baf3a1740382fbc552b1f162f4eb
MattHelton <mjoelhelton@me.com>
2019-01-15 18:49:09 -0500

    Ads ids to all the buttons and creates variable for each, using getElementById.

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-import {add, subtract, multiply, divide, calc, average} from './Modules/Calculator.js'
+import { add, subtract, multiply, divide, calc, average } from './Modules/Calculator.js'
 
 let add = document.getElementById('add')
 let subtract = document.getElementById('subtract')
 let multiply = document.getElementById('multiply')
 let divide = document.getElementById('divide')
+let one = document.getElementById('one')
+let two = document.getElementById('two')
+let three = document.getElementById('three')
+let four = document.getElementById('four')
+let five = document.getElementById('five')
+let six = document.getElementById('six')
+let seven = document.getElementById('seven')
+let eight = document.getElementById('eight')
+let nine = document.getElementById('nine')
+let zero = document.getElementById('zero')
+let point = document.getElementById('point')
+let average = document.getElementById('average')
+let equal = document.getElementById('equal')
+let ac = document.getElementById('ac')
 
 add.addEventListener('click', add())
 subtract.addEventListener('click', subtract())
