effc000a99eb421ed674a7d8ddf5cd56dc250a62
MattHelton <mjoelhelton@me.com>
2019-01-30 01:37:01 -0500

    Addition function now works along with the equals and a/c button.

--- App.js
+++ App.js
@@ -22,79 +22,115 @@ let negPos = document.getElementById('neg-pos')
 let digits = []
 let num1 = 0
 let num2 = 0
+let existingNumbers = []
+let values = []
+let display = document.getElementById('display')
 
+let total = 0
 
-one.addEventListener("click", function(){
-    console.log(1)
-    digits = `${digits}1`
+one.addEventListener("click", function () {
+  console.log(1)
+  digits = `${digits}1`
   console.log(digits)
 })
-two.addEventListener("click", function(){
-    console.log(2)
-    digits = `${digits}2`
+two.addEventListener("click", function () {
+  console.log(2)
+  digits = `${digits}2`
   console.log(digits)
 })
-three.addEventListener("click", function(){
-    console.log(3)
-    digits = `${digits}3`
+three.addEventListener("click", function () {
+  console.log(3)
+  digits = `${digits}3`
   console.log(digits)
 })
-four.addEventListener("click", function(){
-    console.log(4)
-    digits = `${digits}4`
+four.addEventListener("click", function () {
+  console.log(4)
+  digits = `${digits}4`
   console.log(digits)
 })
-five.addEventListener("click", function(){
-    console.log(5)
-    digits = `${digits}5`
+five.addEventListener("click", function () {
+  console.log(5)
+  digits = `${digits}5`
   console.log(digits)
 })
-six.addEventListener("click", function(){
-    console.log(6)
-    digits = `${digits}6`
+six.addEventListener("click", function () {
+  console.log(6)
+  digits = `${digits}6`
   console.log(digits)
 })
-seven.addEventListener("click", function(){
-    console.log(7)
-    digits = `${digits}7`
+seven.addEventListener("click", function () {
+  console.log(7)
+  digits = `${digits}7`
   console.log(digits)
 })
-eight.addEventListener("click", function(){
-    console.log(8)
-    digits = `${digits}8`
+eight.addEventListener("click", function () {
+  console.log(8)
+  digits = `${digits}8`
   console.log(digits)
 })
-nine.addEventListener("click", function(){
-    console.log(9)
-    digits = `${digits}9`
+nine.addEventListener("click", function () {
+  console.log(9)
+  digits.push('9')
   console.log(digits)
 })
-zero.addEventListener("click", function(){
-    console.log(0)
-    digits = `${digits}0`
-    console.log(digits)
+zero.addEventListener("click", function () {
+  console.log(0)
+  digits = `${digits}0`
+  console.log(digits)
 })
 // point.addEventListener("click", point())
-equal.addEventListener("click", function() {
-    num2 = digits
-    
-    let answer = parseInt(num1, 10) + parseInt(num2, 10)
-    console.log(answer)
-    digits = answer
+equal.addEventListener("click", function () {
+  // num2 = digits
+
+  // let answer = parseInt(num1, 10) + parseInt(num2, 10)
+  // console.log(answer)
+  // digits = answer
+  console.log(existingNumbers)
+  existingNumbers.push(digits.join(''))
+  digits = digits.slice(0, 0)
+
+  const numbers = existingNumbers.map(digit => parseInt(digit, 10))
+  const value = numbers.reduce((acc, curr) => {
+    return acc + curr
+  }, 0)
+  existingNumbers = existingNumbers.slice(0, 0)
+  values.push(value)
+  console.log(value)
+  console.log(values)
+
+  const runningTotal = values.reduce((acc, curr) => {
+    return acc + curr
+  }, 0)
+
+  console.log(runningTotal)
 })
-ac.addEventListener("click", function() {
-    digits = 0
-    num1 = 0
-    num2 = 0
+
+ac.addEventListener("click", function () {
+  digits = digits.slice(0, 0)
+  existingNumbers = existingNumbers.slice(0, 0)
+  values = values.slice(0, 0)
+  num1 = 0
+  num2 = 0
 })
-addBtn.addEventListener("click", function() {
-    num1 = digits
-    digits = [] 
-    console.log(num1)
-    console.log(digits)
+addBtn.addEventListener("click", function add(e) {
+  // answer = 0
+  // for (let num of nums) answer += num
+  // return answer
+  console.log(digits)
+  if (digits.length >= 1) {
+    existingNumbers.push(digits.join(''))
+
+    digits = digits.slice(0, 0)
+  } else {
+    existingNumbers.push(0)
+  }
+
+  console.log("digits should be empty: " + digits)
+  console.log("existingNumbers: " + existingNumbers)
+
 })
-negPos,addEventListener("click", function() {
-  if (digits > 0 ) {
+negPos, addEventListener("click", function () {
+  if (digits > 0) {
     return Math.abs(digits) * -1
   } else {
     return Math.abs(digits)
--- Modules/Calculator.js
+++ Modules/Calculator.js
@@ -1,24 +1,27 @@
-export function numberHandler (func) {
+export function numberHandler(func) {
 
 }
 
-export function add (...nums) {
-    answer = 0
-    for (let num of nums) answer += num
-    return answer
+export function add(...nums) {
+    // answer = 0
+    // for (let num of nums) answer += num
+    // return answer
+    const answer = nums.reduce((acc, curr) => {
+        return acc + curr
+    }, 0)
     console.log(answer)
 }
-export function subtract (...nums) {
+export function subtract(...nums) {
     answer = 0
-return num1 - num2
+    return num1 - num2
 }
-export function multiply (num1, num2) {
+export function multiply(num1, num2) {
     return num1 * num2
 }
-export function divide (num1, num2) {
+export function divide(num1, num2) {
     return num1 / num2
 }
-export function average (...nums) {
+export function average(...nums) {
     let sum = 0
     let numNums = 0
     for (let num of nums) {
@@ -27,11 +30,11 @@ export function average (...nums) {
     }
     return sum / numNums
 }
-export function calc (callback, ...nums) {
+export function calc(callback, ...nums) {
     return callback(...nums)
 }
 
-export function number (number) {
+export function number(number) {
     return number
     console.log(number)
 }
